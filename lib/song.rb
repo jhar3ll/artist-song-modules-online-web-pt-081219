c5e3ca8ebacf352407f89aad9a8d1b0e7a28c53d
Jharell Chambers <jharell.chambers@selu.edu>
2019-09-04 01:43:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 class Song
+  extend Memorable
   attr_accessor :name
   attr_reader :artist
 
@@ -18,9 +19,7 @@ class Song
     @@songs
   end
 
- # def self.reset_all
-  #  self.all.clear
-  #end
+ 
 
   #def self.count
   #  self.all.count
